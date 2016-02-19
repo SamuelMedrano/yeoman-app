12ec7e8eefceaee6ea149588f5fcafdfa3c7040a
SamuelMedrano <smlmedrano@gmail.com>
2016-02-19 15:17:25 -0800

    Add

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yeomanAppApp
+ */
+angular.module('yeomanAppApp')
+	 .controller('MainCtrl', function ($scope, localStorageService) {
+
+    var todosInStore = localStorageService.get('todos');
+	
+    $scope.todos = todosInStore || [];
+    $scope.text = 'kill';
+    $scope.$watch('todos', function () {
+      localStorageService.set('todos', $scope.todos);
+    }, true);
+
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
+    $scope.editTodo = function(index) {
+     $scope.todos[index] = $scope.text;
+
+
+    };
+
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
+    $scope.removeTodoAll = function () {
+     	$scope.todos = [];
+    };
+  
+
+  });
